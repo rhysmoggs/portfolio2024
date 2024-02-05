9a422254c34623f6ada47035cfd6a90aa83d2038
Rhys Morgan <rhysmoggs@hotmail.com>
2024-02-05 12:40:09 +0000

    add buttons to HeroSection and styling

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -13,10 +13,14 @@ const HeroSection = () => {
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Provident,
             numquam.
           </p>
-        </div>
-        <div>
-          <button className='px-6 py-3 rounded-full mr-4'>Hire Me</button>
-          <button className='px-6 py-3 rounded-full mr-4'>Download CV</button>
+          <div>
+            <button className='px-6 py-3 rounded-full mr-4 bg-white hover:bg-slate-200 text-black'>
+              Hire Me
+            </button>
+            <button className='px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white mt-3'>
+              Download CV
+            </button>
+          </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
           <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden'>
