5efa569cfea3defaf69dd9563ac18e412b802f9c
Rhys Morgan <rhysmoggs@hotmail.com>
2024-02-06 14:52:39 +0000

    add button state to show/hide icons on click

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <main className='flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12'>
+    <main className='flex min-h-screen flex-col bg-[#121212]'>
       <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
