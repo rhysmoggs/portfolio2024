41d265111dbef83e19efcb6a3bfb13437ce1323e
Rhys Morgan <rhysmoggs@hotmail.com>
2024-02-06 09:01:41 +0000

    create Navbar component and dropdown Link

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -39,7 +39,7 @@ const HeroSection = () => {
             <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-slate-100 via-pink-400 to-red-500 hover:bg-slate-200 text-black'>
               Hire Me
             </button>
-            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-slate-100 via-pink-400 to-red-500 hover:bg-slate-800 mt-3'>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-slate-100 via-pink-400 to-red-500 hover:bg-slate-800 text-white mt-3'>
               <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
                 Download CV
               </span>
--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import HeroSection from './components/HeroSection'
+import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col bg-[#121212] container mx-auto py-4 px-12'>
+      <Navbar />
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
       </div>
