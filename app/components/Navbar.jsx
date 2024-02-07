bf261656fc72b1fcea923e78a00fcfcbf244f238
Rhys Morgan <rhysmoggs@hotmail.com>
2024-02-07 10:58:24 +0000

    add MenuOverlay component for mobile menu

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import React, { useState } from 'react'
 import NavLink from './NavLink'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid'
+import MenuOverlay from './MenuOverlay'
 
 const navLinks = [
   {
@@ -57,6 +58,7 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
     </nav>
   )
 }
