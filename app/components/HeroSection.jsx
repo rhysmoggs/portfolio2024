95572e985dae5898f9ef1b1725877480fad517d1
Rhys Morgan <rhysmoggs@hotmail.com>
2024-02-20 21:18:15 +0000

    add styling to navbar, responsive hero section and open external links to new tabs

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -6,10 +6,10 @@ import { TypeAnimation } from 'react-type-animation'
 
 const HeroSection = () => {
   return (
-    <section>
+    <section className='lg:py-16'>
       <div className='grid grid-cols-1 sm:grid-cols-12'>
-        <div className='col-span-7 place-self-center text-center sm:text-left'>
-          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
+        <div className='col-span-8 place-self-center text-center sm:text-left justify-self-start'>
+          <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold'>
             <span className='text-transparent bg-clip-text bg-gradient-to-r from-slate-100 to-red-500'>
               Hello, I&apos;m{' '}
             </span>
@@ -39,12 +39,14 @@ const HeroSection = () => {
           <div>
             <Link
               href={'https://www.linkedin.com/in/rhysmoggs/'}
+              target='_blank'
               className='px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-slate-100 to-red-500 hover:bg-slate-200 text-black'
             >
               Hire Me @ LinkedIn
             </Link>
             <Link
               href={'https://github.com/rhysmoggs'}
+              target='_blank'
               className='px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-slate-100 to-red-500 hover:bg-slate-800 text-white mt-3'
             >
               <span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>
@@ -53,7 +55,7 @@ const HeroSection = () => {
             </Link>
           </div>
         </div>
-        <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
+        <div className='col-span-4 place-self-center mt-4 lg:mt-0'>
           <div className='rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden'>
             <Image
               src='/images/headshot.jpg'
