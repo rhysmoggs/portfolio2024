a43949220ce52580dc6dec8026e4623ea55f36ec
Rhys Morgan <rhysmoggs@hotmail.com>
2024-03-11 12:51:23 +0000

    add animation to navbar hover

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const NavLink = ({ href, title, pageNo }) => {
   return (
@@ -9,9 +10,14 @@ const NavLink = ({ href, title, pageNo }) => {
         className='block py-2 text-black sm:text-xl rounded hover:text-white'
       >
         <h1 className='text-white text-xl'>{0 + `${pageNo}`}</h1>
-        <h1 className='text-white md:font-medium xl:font-semibold text-2xl md:text-4xl lg:text-5xl'>
+        <motion.h1
+          whileHover={{ scale: 1.15 }}
+          initial={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+          className='text-white md:font-medium xl:font-semibold text-2xl md:text-4xl lg:text-5xl'
+        >
           {title}
-        </h1>
+        </motion.h1>
         <span className='block text-xs text-secondary-100 text-center'>
           ___________
         </span>
