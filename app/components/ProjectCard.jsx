0cfa8d8042a74162ab8cad6ab76603322b4d86ca
Rhys Morgan <rhysmoggs@hotmail.com>
2024-11-11 18:22:12 +0000

    remove link on ProjectCard for about page link

--- app/components/ProjectCard.jsx
+++ app/components/ProjectCard.jsx
@@ -19,12 +19,12 @@ function ProjectCard({ image, title, description, techstack, github }) {
           >
             <CodeBracketIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:white' />
           </Link>
-          <Link
+          {/* <Link
             href='/about'
             className='h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link'
           >
             <EyeIcon className='h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer group-hover/link:white' />
-          </Link>
+          </Link> */}
         </div>
       </div>
       <motion.div
